e8446a4a2504d30706b0ef6ff87616e0eaf86755
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-06-09 19:56:48 +0800

    Feat: create initial structure for get_pages_data()

--- src/default_units.py
+++ src/default_units.py
@@ -2,6 +2,7 @@
 
 import json
 import os
+from pprint import pprint
 
 
 def get_default_units():
@@ -16,10 +17,43 @@ def get_default_units():
     return data
 
 
-def get_page_data(data, idx=0):
-    page = data[idx]
-    {
-        "contents": {
-            "a": page["name"],
+def get_pages_data(data):
+    pages = []
+    for idx, unit in enumerate(data):
+        page = {}
+        page["name"] = unit["name"]
+        page["idx"] = idx
+
+        page["traits"] = []
+        for trait in unit["traits"]:
+            perTrait = {}
+            perTrait["trait"] = trait["trait"]
+            perTrait["abilities"] = trait["abilities"]
+
+            perStat = {
+                "HP": trait["stats"]["health"],
+                "DEF": trait["stats"]["defence"],
+                "ATK": trait["stats"]["attack"]
+            }
+            perTrait["stats"] = perStat
+
+            page["traits"].append(perTrait)
+
+        pages.append(page)
+        
+    [
+        {
+            "name": "Archer",
+            "idx": 1,
+            "traits": [
+                {
+                "trait": "Normal",
+                "stats": {
+                    "HP": 100,
+                    "DEF": 10,
+                    "ATK": 35
+                    }
+                },
+            ]
         }
-    }
+    ]
