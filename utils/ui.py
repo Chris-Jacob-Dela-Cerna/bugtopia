47a1472567cc9662ecdeba31f8bffe10bac8eafc
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-06-10 11:46:30 +0800

    Feat: adjust code and initial clean up for convert_data_ui_player()

--- utils/ui.py
+++ utils/ui.py
@@ -16,14 +16,14 @@ def show_title():
 
 def convert_data_ui_menu(ui_data):
     ui = []
-    header = ui_data["header"]
-    header_space = fit(28, ui_data["header"])
+    header = ui_data['header']
+    header_space = fit(28, ui_data['header'])
     ui.append("               ╭──────────────────────────────╮")
     ui.append(f"               │ {header}{header_space} |")
     ui.append("               |──────────────────────────────|")
 
     blank_option = 5
-    options = ui_data["options"].items()
+    options = ui_data['options'].items()
     for letter, option in options:
         blank_option -= 1
         option_space = fit(25, option)
@@ -35,31 +35,49 @@ def convert_data_ui_menu(ui_data):
     return ui
 
 
-def convert_data_ui_player(ui_data, idx=0, deck=[]):
+def convert_data_ui_player(ui_data, idx=1, deck=[]):
     page = ui_data[idx]
 
     ui = []
     ui.append("  ╭────────────────────────────────────────────────────────╮")
     ui.append("  | Create your deck:                                      |")
     ui.append("  |────────────────────────────────────────────────────────|")
-    ui.append("  | " + str(page["idx"] + 1) + ") " + page["name"] + ((((52 - len(page["name"])) - len(str(page["idx"])))) * " ") + " |")
+
+    page_idx = page['idx'] + 1
+    page_name = page['name']
+    unit_space = fit(52, f"{page_idx}{page_name}")
+    ui.append(f"  | {page_idx}) {page_name}{unit_space} |")
     ui.append("  | Traits:                                                |")
 
-    letters = "abc"
+    options = "abc"
+    rows = ["  |", "  |", "  |", "  |"]
     rowTrait = "  |"
     rowHp = "  |"
     rowDef = "  |"
     rowAtk = "  |"
-    listAbi = []
+    ability_list = []
+
+
+
     for x in range(3):
-        curTrait = page["traits"][x]
-        rowTrait += " " + letters[x] + ". " + curTrait["trait"].title() + ((13 - len(curTrait["trait"])) * " ") + " |"
-        curStat = curTrait["stats"]
-        rowHp += " " + "HP: " + str(curStat["health"]) + ((12 - len(str(curStat["health"]))) * " ") + " |"
-        rowDef += " " + "DEF: " + str(curStat["defence"]) + ((11 - len(str(curStat["defence"]))) * " ") + " |"
-        rowAtk += " " + "ATK: " + str(curStat["attack"]) + ((11 - len(str(curStat["attack"]))) * " ") + " |"
-        tempAbi = [_ for _ in curTrait["abilities"]]
-        listAbi.append(tempAbi)
+        current_trait = page['traits'][x]
+        trait = current_trait['trait']
+        trait_space = fit(13, trait)
+        rowTrait += f" {options[x]}. {trait.title()}{trait_space} |"
+
+        health = current_trait['stats']['health']
+        defence = current_trait['stats']['defence']
+        attack = current_trait['stats']['attack']
+        health_space = fit(12, health)
+        defence_space = fit(11, defence)
+        attack_space = fit(11, attack)
+        rowHp += f" HP: {health}{health_space} |"
+        rowDef += f" DEF: {defence}{defence_space} |"
+        rowAtk += f" ATK: {attack}{attack_space} |"
+
+        abilities = current_trait['abilities']
+        ability_list.append([_ for _ in abilities])
+
     ui.append(rowTrait)
     ui.append(rowHp)
     ui.append(rowDef)
@@ -68,11 +86,11 @@ def convert_data_ui_player(ui_data, idx=0, deck=[]):
     ui.append("  | Abilities:                                             |")
     displayAbi = ["  |", "  |", "  |", "  |"]
     for z in range(3):
-        for y in range(len(listAbi[z])):
-            if len(listAbi[z][y]) == 0:
+        for y in range(len(ability_list[z])):
+            if len(ability_list[z][y]) == 0:
                 displayAbi[y] += "                  |"
                 continue
-            displayAbi[y] += " - " + listAbi[z][y] + ((14 - len(listAbi[z][y])) * " ") + " |"
+            displayAbi[y] += " - " + ability_list[z][y] + ((14 - len(ability_list[z][y])) * " ") + " |"
     for ability in displayAbi:
         ui.append(ability)
     ui.append("  |────────────────────────────────────────────────────────|")
@@ -82,7 +100,7 @@ def convert_data_ui_player(ui_data, idx=0, deck=[]):
 
 
 def fit(max_length, text_length):
-    return (max_length - len(text_length)) * " "
+    return (max_length - len(str(text_length))) * " "
 
 
 def display(ui):
