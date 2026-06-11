b2da431e67384dbae5a5b6baa8d6b20ff9acb087
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-06-11 22:56:01 +0800

    Feat: merge both player menus in one reusable function

--- src/main_logic.py
+++ src/main_logic.py
@@ -26,34 +26,14 @@ def main_menu():
 
 
 def player_deck_creation():
-    if not (deck_1 := player_menu_1()):
+    if not (deck_1 := player_menu(1)):
         return
-    if not (deck_2 := player_menu_2()):
+    if not (deck_2 := player_menu(2)):
         return
 
 
-def player_menu_1():
-    player_menu_data = mn.get_player_menu(1)
-    player_menu_ui = um.convert_data_ui_menu(player_menu_data)
-
-    while True:
-        uh.display(player_menu_ui)
-        chosen = input("                 >>> ").strip()
-        if chosen in player_menu_data["options"]:
-            route = {
-            "a": cd.create_deck,
-            "b": lambda: print("To be implemented."),
-            "c": lambda: "Back"
-            }
-            result = route[chosen]()
-            if result == "Back":
-                break
-            elif result:
-                return result
-
-
-def player_menu_2():
-    player_menu_data = mn.get_player_menu(2)
+def player_menu(player_number):
+    player_menu_data = mn.get_player_menu(player_number)
     player_menu_ui = um.convert_data_ui_menu(player_menu_data)
 
     while True:
