e4eecc332ea97f9ded3e3e70d04e52be6c57b00a
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-06-14 20:42:01 +0800

    Feat: reduce for loop repetitiveness with loop iterator and if chains

--- ui/battle_screen.py
+++ ui/battle_screen.py
@@ -9,39 +9,6 @@ def convert_battle_ui(deck1, deck2):
     ui.append("  | Player 1                                               |")
     ui.append("  |────────────────────────────────────────────────────────|")
 
-    unit_row = "  | "
-    for idx in range(len(deck1)):
-        if deck1[idx] == None:
-            unit_row += "--            -- | "
-        else:
-            visible_idx = idx + 1
-            unit_name = deck1[idx].unit
-            unit_space = fit(13, unit_name)
-            unit_row += f"{visible_idx}) {unit_name}{unit_space} | "
-    ui.append(unit_row)
-
-    trait_row = "  | "
-    for idx in range(len(deck1)):
-        if deck1[idx] == None:
-            trait_row += "   --      --    | "
-        else:
-            trait_name = deck1[idx].trait
-            trait_space = fit(13, trait_name)
-            trait_row += f"T: {trait_name}{trait_space} | "
-    ui.append(trait_row)
-
-    health_row = "  | "
-    for idx in range(len(deck1)):
-        if deck1[idx] == None:
-            health_row += "       --        | "
-        else:
-            health = deck1[idx].health
-            base_health = deck1[idx].base_health
-            health_space = fit(13, f"{health}/{base_health}")
-            health_row += f"H: {health}/{base_health}{health_space} | "
-    ui.append(health_row)
-
-
     rows = ["  | ", "  | ", "  | ", "  | ", "  | ", "  | "]
     dead_rows = [
         "--            -- | ",
@@ -57,7 +24,30 @@ def convert_battle_ui(deck1, deck2):
             if slot == None:
                 row += dead_rows[x]
                 continue
-            row += "                 | "
+            elif x == 0:
+                visible_idx = y + 1
+                unit_name = slot.unit
+                unit_space = fit(13, unit_name)
+                row += f"{visible_idx}) {unit_name}{unit_space} | "
+            elif x == 1:
+                trait_name = slot.trait
+                trait_space = fit(13, trait_name)
+                row += f"T: {trait_name}{trait_space} | "
+            elif x == 2:
+                health = slot.health
+                base_health = slot.base_health
+                health_space = fit(13, f"{health}/{base_health}")
+                row += f"H: {health}/{base_health}{health_space} | "
+            elif x == 3:
+                defence = slot.defence
+                defence_space = fit(11, defence)
+                row += f"DEF: {defence}{defence_space} | "
+            elif x == 4:
+                attack = slot.attack
+                attack_space = fit(11, attack)
+                row += f"ATK: {attack}{attack_space} | "
+            else:
+                row += "                 | "
             rows[x] = row
 
     ui.extend(rows)
