537e9bf206096b389a8bd795337b9aaf4edf98bb
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-06-14 19:52:45 +0800

    Feat: add for loops to iterate through traits and hp

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     from utils import ui_helpers as uh
 
     units_data = aj.load_json_data("units_default.json")
-    deck1 = [Unit(0, 0, units_data), Unit(2, 0, units_data), Unit(2, 0, units_data)]
+    deck1 = [Unit(0, 0, units_data), None, Unit(2, 0, units_data)]
     deck2 = [Unit(0, 2, units_data), Unit(1, 2, units_data), Unit(2, 2, units_data)]
 
     battle_ui = bs.convert_battle_ui(deck1, deck2)
--- ui/battle_screen.py
+++ ui/battle_screen.py
@@ -9,7 +9,37 @@ def convert_battle_ui(deck1, deck2):
     ui.append("  | Player 1                                               |")
     ui.append("  |────────────────────────────────────────────────────────|")
 
+    unit_row = "  | "
+    for idx in range(len(deck1)):
+        if deck1[idx] == None:
+            unit_row += "--            -- | "
+        else:
+            visible_idx = idx + 1
+            unit_name = deck1[idx].unit
+            unit_space = fit(13, unit_name)
+            unit_row += f"{visible_idx}) {unit_name}{unit_space} | "
+    ui.append(unit_row)
 
+    trait_row = "  | "
+    for idx in range(len(deck1)):
+        if deck1[idx] == None:
+            trait_row += "   --      --    | "
+        else:
+            trait_name = deck1[idx].trait
+            trait_space = fit(13, trait_name)
+            trait_row += f"T: {trait_name}{trait_space} | "
+    ui.append(trait_row)
+
+    health_row = "  | "
+    for idx in range(len(deck1)):
+        if deck1[idx] == None:
+            health_row += "       --        | "
+        else:
+            health = deck1[idx].health
+            base_health = deck1[idx].base_health
+            health_space = fit(13, f"{health}/{base_health}")
+            health_row += f"H: {health}/{base_health}{health_space} | "
+    ui.append(health_row)
 
     ui.append("  |────────────────────────────────────────────────────────|")
     ui.append("  |                                                        |")
