322cbdbc861d5d806de23535a1e11302a65288d1
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-06-14 19:08:16 +0800

    Feat: add temporary code in main.py to test battle_screen.py

--- main.py
+++ main.py
@@ -4,7 +4,20 @@ from src import main_logic
 
 
 def main():
-    main_logic.main_menu()
+    #Temporary
+    from ui import battle_screen as bs
+    from src.unit_class import Unit
+    from utils import access_json as aj
+    from utils import ui_helpers as uh
+
+    units_data = aj.load_json_data("units_default.json")
+    deck1 = [Unit(0, 0, units_data), Unit(2, 0, units_data), Unit(2, 0, units_data)]
+    deck2 = [Unit(0, 2, units_data), Unit(1, 2, units_data), Unit(2, 2, units_data)]
+
+    battle_ui = bs.convert_battle_ui(deck1, deck2)
+    uh.display(battle_ui)
+
+    # main_logic.main_menu()
 
 
 if __name__ == "__main__":
