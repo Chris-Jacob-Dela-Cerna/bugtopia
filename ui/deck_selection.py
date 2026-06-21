28def08f5c2ad85d929f11f3699bdd2c786bbb6e
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-06-21 20:35:38 +0800

    Feat: add get_help_state for displaying help text

--- ui/deck_selection.py
+++ ui/deck_selection.py
@@ -3,7 +3,7 @@
 from utils.ui_helpers import fit
 
 
-def render_deck_ui():
+def render_deck_ui(show_help):
     ui = []
     ui.append(f"{"\n" * 35}")
     ui.append("           ╭──────────────────────────────────────╮")
@@ -16,3 +16,17 @@ def render_deck_ui():
     ui.append("           |──────────────────────────────────────|")
 
     ui.append("           ╰──────────────────────────────────────╯")
+
+    ui.extend(get_help_state(show_help))
+    return ui
+
+
+def get_help_state(show_help):
+    if show_help:
+        return [
+            "             To change page -> type 'q', 'e'"
+            "             To select -> type 'f'"
+            "             To go back -> type 'b'"
+        ]
+    else:
+        return ["             Type 'h' to view all options."]
